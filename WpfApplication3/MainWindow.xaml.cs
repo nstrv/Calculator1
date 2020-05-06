75c6a7728a5ebf17bb4459d43daa5ac68679ff90
Николай Нестеров <61569894+nstrv@users.noreply.github.com>
2020-05-06 14:58:04 +1000

    Add files via upload

--- WpfApplication3/MainWindow.xaml.cs
+++ WpfApplication3/MainWindow.xaml.cs
@@ -21,25 +21,38 @@ namespace Калькулятор
     public partial class MainWindow : Window
     {
         public string operation;
-        public string right;
-        public bool boolleft;
+        public string operation2;
+        public string left;
+        public bool boolright;
         public string ms;
+        public int bl = 0;
+        public int del = 0; // нажатие /
+        public bool num = false; //нажатие 0-9
+        public bool sq = false; //нажатие sqrt
+        public bool delx = false; // нажатие 1\х
+        public bool kv = false; // нажатие sqr
+        public string pez = ""; //копирует textBox1.Text после нажатия =
+        public bool rav = false; // нажатие =
+        public bool proc; // нажатие %
 
         public MainWindow()
         
         {
-            boolleft = false;
+            boolright = false;
             InitializeComponent();
            
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e) //кнопки от 0 до 9
         {
-            if (textBox1.Text == "M")
+            if (textBox1.Text == "∞") 
                 textBox1.Text = "";
-            if (boolleft == true)
+
+            if (textBox1.Text == "бесконечность")
+                textBox1.Text = "";
+            if (boolright == true)
             {
-                boolleft = false;
+                boolright = false;
                 textBox1.Text = "";
             }
             Button B = (Button)sender;
@@ -47,123 +60,197 @@ namespace Калькулятор
                 textBox1.Text = B.Content.ToString();
             else
                 textBox1.Text += B.Content.ToString();
+            
+            if (boolright == true)
+            {
+                boolright = false;
+                textBox2.Text = "";
+            }
+            Button B1 = (Button)sender;
+            if ((textBox2.Text == "0") && (sq == false) && (kv == false) && (delx == false))
+                textBox2.Text = B1.Content.ToString();
+            if ((textBox2.Text != "0") && (sq == false) && (kv == false) && (delx == false))
+                textBox2.Text += B1.Content.ToString();
+            num = true;
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e) //очистка
         {
                 textBox1.Text = "0";
+                textBox2.Text = "";
+                bl = 0;
+                pez = "";
+                sq = false;
+                kv = false;
+                delx = false;
+                proc = false;
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e) //операции
         {
-            if (textBox1.Text == "M")
-                textBox1.Text = "0";
-            Button B = (Button)sender;
-            operation = B.Content.ToString();
-            right = textBox1.Text;
-            boolleft = true;
+            if ((textBox2.Text == "") && (textBox1.Text == "0"))
+                textBox2.Text = "0";
+            if ((textBox2.Text == "") && (textBox1.Text != "0"))
+                textBox2.Text = textBox1.Text;
+
+            if ((sq == false) && (kv == false) && (delx == false))
+            {
+                Button B = (Button)sender;
+                operation = B.Content.ToString();
+                left = textBox1.Text;
+                boolright = true;
+
+                if (bl == 0)
+                {
+                    bl++;
+                    textBox2.Text += operation;
+                }
+            }
+            
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e) //результат
         {
-            if (textBox1.Text == "M")
-                textBox1.Text = "0";
+            
             double db1, db2, result;
             result = 0;
-            db1 = Convert.ToDouble(right);
+            db1 = Convert.ToDouble(left);
             db2 = Convert.ToDouble(textBox1.Text);
+           
             if (operation == "+")
             {
-                result = db1 + db2;
+               textBox2.Text = "";
+               result = db1 + db2;
+
+               if (proc == true)
+               {
+                 textBox2.Text = "";
+                 result = db1 + (db1 * db2/100);
+               }
             }
             if (operation == "-")
             {
+                textBox2.Text = "";
                 result = db1 - db2;
+                if (proc == true)
+                {
+                    textBox2.Text = "";
+                    result = db1 - (db1 * db2 / 100);
+                }
             }
             if (operation == "*")
             {
+                textBox2.Text = "";
                 result = db1 * db2;
+                if (proc == true)
+                {
+                    textBox2.Text = "";
+                    result = db1 * (db1 * db2 / 100);
+                }
             }
             if (operation == "/")
             {
+                textBox2.Text = "";
                 result = db1 / db2;
-            }
-            if (operation == "%")
-            {
-                result = db1 * db2/ 100;
+                if (proc == true)
+                {
+                    textBox2.Text = "";
+                    result = db1 / (db1 * db2 / 100);
+                }
             }
             operation = "=";
-            boolleft = true;
+            boolright = true;
+            bl = 0;
             textBox1.Text = result.ToString();
+            pez = textBox1.Text;
+            rav = true;
+
         }
 
         private void Button_Click_5(object sender, RoutedEventArgs e) //извлечение квадратного корня
         {
+            textBox2.Text = "";
             double db, result;
-
+            textBox2.Text = textBox2.Text.Insert(0, "sqrt(");
+            textBox2.Text += textBox1.Text;
+            textBox2.Text += ")";
             db = Convert.ToDouble(textBox1.Text);
             result = Math.Sqrt(db);
             textBox1.Text = result.ToString();
+            pez = textBox1.Text;
+            sq = true;
            
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e) //возведение в квадрат
         {
-            if (textBox1.Text == "M")
-                textBox1.Text = "0";
+            textBox2.Text = "";
             double db, result;
-
+            textBox2.Text = textBox2.Text.Insert(0, "sqr(");
+            textBox2.Text += textBox1.Text;
+            textBox2.Text += ")";
             db = Convert.ToDouble(textBox1.Text);
             result = Math.Pow(db, 2);
             textBox1.Text = result.ToString();
+            kv = true;
         }
 
         private void Button_Click_7(object sender, RoutedEventArgs e) // 1\х
         {
-            if (textBox1.Text == "M")
+            textBox2.Text = "";
+            if (textBox1.Text == "∞")
+                textBox1.Text = "0";
+            if (textBox1.Text == "бесконечность")
                 textBox1.Text = "0";
             double db, result;
-
+            textBox2.Text = textBox2.Text.Insert(0, "reciproc(");
+            textBox2.Text += textBox1.Text;
+            textBox2.Text += ")";
             db = Convert.ToDouble(textBox1.Text);
             result = 1 / db;
             textBox1.Text = result.ToString();
+            delx = true;
         }
 
         private void Button_Click_8(object sender, RoutedEventArgs e) //изменение знака
         {
-            if (textBox1.Text == "M")
-                textBox1.Text = "0";
+            
             double db, result;
-
             db = Convert.ToDouble(textBox1.Text);
             result = -db;
             textBox1.Text = result.ToString();
+            textBox2.Text = result.ToString();
         }
 
         private void Button_Click_9(object sender, RoutedEventArgs e) //запятая
         {
-            if (textBox1.Text == "M")
-                textBox1.Text = "0";
+            
             if (!textBox1.Text.Contains(","))
+            {
                 textBox1.Text += ",";
+                textBox2.Text += ",";
+            }
         }
 
         private void Button_Click_10(object sender, RoutedEventArgs e) //удаление последней цифры
         {
+
             textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            if ((sq == false) && (kv == false) && (delx == false) && (textBox2.Text.Length >= 0))
+                textBox2.Text = textBox2.Text.Substring(0, textBox2.Text.Length - 1);
             if (textBox1.Text == "")
                 textBox1.Text = "0";
+            if (textBox2.Text == "")
+                textBox2.Text = "0";
         }
 
         private void Button_Click_11(object sender, RoutedEventArgs e) //MS
         {
             ms = textBox1.Text;
-            textBox1.Text = "M";
         }
 
         private void Button_Click_12(object sender, RoutedEventArgs e) //MR
         {
-            
             textBox1.Text = ms;
         }
 
@@ -171,6 +258,7 @@ namespace Калькулятор
         {
             ms = "0";
             textBox1.Text = "0";
+            textBox2.Text = "";
         }
 
         private void Button_Click_14(object sender, RoutedEventArgs e) //M+
@@ -189,6 +277,11 @@ namespace Калькулятор
             ms = Convert.ToString(a2);
         }
 
-        
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+            proc = true;
+            if ((textBox1.Text != "бесконечность") || (textBox1.Text == "∞"))
+                textBox2.Text += "%";
+        }
     }
 }
